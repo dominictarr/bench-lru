96b6290a52725500ed2c6e6ac6776af3c6bb424a
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-11-12 15:27:23 -0500

    Reporting ops per sec based on median of results, should we lint?
    Fixing sort by sorting `Number` primitives, removing unneeded modules
    
    Ops/sec? i haven't eaten in like 12hr so i dunno :D
    
    better...

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-'use strict'
+'use strict';
 
 const Worker = require('tiny-worker'),
   ora = require('ora'),
   caches = [
-    'lru-cache',
-    'lru-fast',
-    'js-lru',
-    'modern-lru',
-    'quick-lru',
-    'secondary-cache',
-    'simple-lru-cache',
-    'tiny-lru',
     'hashlru',
-    'hyperlru-object',
     'hyperlru-map',
-    'lru_cache',
+    'hyperlru-object',
+    'js-lru',
     'lru',
-    'mkc'
-  ],
-  total = caches.length;
+    //'lru-cache', // slow
+    'lru-fast',
+    'lru_cache',
+    //'mkc', // slow
+    //'modern-lru', // slow
+    'quick-lru',
+    //'secondary-cache', // slow
+    'simple-lru-cache',
+    'tiny-lru'
+  ];
 
-const spinner = ora(`Benchmarking ${total} caches`).start();
+const spinner = ora(`Benchmarking ${caches.length} caches`).start();
 
 Promise.all(caches.map(i => {
   return new Promise(resolve => {
@@ -31,6 +30,11 @@ Promise.all(caches.map(i => {
       worker.terminate();
     };
 
+    worker.onerror = ev => {
+      console.error(ev.data || ev);
+      process.exit(1);
+    };
+
     worker.postMessage(i);
   });
 })).then(results => {
@@ -38,5 +42,8 @@ Promise.all(caches.map(i => {
     keysort = require('keysort');
 
   spinner.stop();
-  console.log(toMD(['name,set,get1,update,get2,evict'].concat(keysort(results.map(i => JSON.parse(i)), 'evict, set, get1, update, get2').map(i => `${i.name},${i.set},${i.get1},${i.update},${i.get2},${i.evict}`)).join('\n')));
-}).catch(err => console.error(err.stack || err.message || err));
+  console.log(toMD(['name,set,get1,update,get2,evict'].concat(keysort(results.map(i => JSON.parse(i)), 'evict desc, set desc, get1 desc, update desc, get2 desc').map(i => `${i.name},${i.set},${i.get1},${i.update},${i.get2},${i.evict}`)).join('\n')));
+}).catch(err => {
+  console.error(err.stack || err.message || err);
+  process.exit(1);
+});
--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const precise = require('precise'),
-  path = require('path'),
+  retsu = require('retsu'),
   LRUCache = require('lru_cache').LRUCache,
   Simple = require('simple-lru-cache'),
   Fast = require('lru-fast').LRUCache,
@@ -12,7 +12,7 @@ const precise = require('precise'),
   MKC = require('mkc'),
   hyperlruObject = hyperlru(require('hyperlru-object')),
   hyperlruMap = hyperlru(require('hyperlru-map')),
-  init = {
+  caches = {
     'lru-cache': () => require('lru-cache'),
     'lru-fast': n => new Fast(n),
     'js-lru': n => new LRUMap(n),
@@ -28,22 +28,58 @@ const precise = require('precise'),
     lru: require('lru'),
     mkc: max => new MKC({max})
   },
-  iterations = 200000,
-  times = 5;
+  n = 2e5,
+  e = n * 2,
+  t = 5,
+  x = 1e6,
+  s = 1e3;
 
 self.onmessage = function (ev) {
   const id = ev.data,
-    timer = precise().start(),
+    lru = caches[id](n),
+    time = {
+      'set': [],
+      get1: [],
+      update: [],
+      get2: [],
+      evict: []
+    },
     results = {
       name: id,
       'set': 0,
       get1: 0,
       update: 0,
       get2: 0,
-      evict: 0
-  };
+      evict: 0,
+    };
 
-  results.set = timer.stop().diff() / 1e6;
+  let n = -1;
+
+  while (++n < t) {
+    let stimer = precise().start();
+    for (let i = 0; i < n; i++) lru.set(i, Math.random());
+    time.set.push(stimer.stop().diff() / x);
+
+    let gtimer = precise().start();
+    for (let i = 0; i < n; i++) lru.get(i);
+    time.get1.push(gtimer.stop().diff() / x);
+
+    let utimer = precise().start();
+    for (let i = 0; i < n; i++) lru.set(i, Math.random());
+    time.update.push(utimer.stop().diff() / x);
+
+    const g2timer = precise().start();
+    for (let i = 0; i < n; i++) lru.get(i);
+    time.get2.push(g2timer.stop().diff() / x);
+
+    let etimer = precise().start();
+    for (let i = n; i < e; i++) lru.set(i, Math.random());
+    time.evict.push(etimer.stop().diff() / x);
+  }
+
+  ['set', 'get1', 'update', 'get2', 'evict'].forEach(i => {
+    results[i] = (n / retsu.median(time[i]).toFixed(2)).toFixed(0) * s;
+  });
 
   postMessage(JSON.stringify(results));
 };
